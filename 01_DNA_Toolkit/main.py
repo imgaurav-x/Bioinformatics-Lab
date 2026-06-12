994d51f07b1fb262be54b92ad88f63c9a9a1ba2f
imgaurav-x <gaurav.aryan4707@gmail.com>
2026-06-12 18:23:08 +0530

    Add sequence lenght and GC content analysis

--- 01_DNA_Toolkit/main.py
+++ 01_DNA_Toolkit/main.py
@@ -2,6 +2,10 @@
 dna = input("Enter DNA sequence: ").upper() # used to uppercase the string
 print(dna)
 
+# Finding  the length of sequence
+dna_length = len(dna)
+print("Length of DNA sequence:", dna_length)
+
 # Counting the number of each nucleotides
 a_count = dna.count("A")
 t_count = dna.count("T")
@@ -12,3 +16,8 @@ print("Number of A's:",  a_count )
 print("Number of T's:", t_count )
 print("Number of G's:", g_count )
 print("Number of C's:", c_count )
+
+# Calculating the GC content
+gc_cont = g_count + c_count
+gc_content_percentage = (gc_cont / dna_length) * 100
+print("GC Content Percentage:", gc_content_percentage, "%")
